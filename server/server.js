406d08ba68355351a9a96ea5ba4e7009234bc35e
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-03-10 18:20:33 +0000

    Prep for CI/CD on Heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const bugs = require("./bugData.json");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -20,6 +22,10 @@ app.get("/bugs", (req, res) => {
 
 app.get("/bugs/:program", (req, res) => {
   res.status(200).json(bugs[req.params.program]);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
