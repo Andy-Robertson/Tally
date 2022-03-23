3c0457ab34af2f9e2aa3f7bd39d8b31d4ebe87e7
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-03-23 20:35:54 +0000

    Update database tables

--- server/tables.sql
+++ server/tables.sql
@@ -1,31 +1,30 @@
 
-DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS user_details CASCADE;
 DROP TABLE IF EXISTS tracked_application CASCADE;
 DROP TABLE IF EXISTS bug CASCADE;
 
-CREATE TABLE user (
+CREATE TABLE user_details (
   id                        SERIAL PRIMARY KEY,
-  name                      VARCHAR(300) NOT NULL,
+  name                      VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE tracked_application (
   id                        SERIAL PRIMARY KEY,
-  user_id                   INTEGER NOT NULL
+  user_id                   INTEGER NOT NULL,
   name                      VARCHAR(300) NOT NULL,
   url                       VARCHAR(600),
   description               VARCHAR(1000),
-  FOREIGN KEY (user_id)     REFERENCES user(id)
+  FOREIGN KEY(user_id)     REFERENCES user_details(id)
 );
 
 CREATE TABLE bug (
   id                        SERIAL PRIMARY KEY,
-  created_by_user_id        INTEGER NOT NULL
+  created_by_user_id        INTEGER NOT NULL,
   name                      VARCHAR(300) NOT NULL,
   description               VARCHAR(1500),
   recreation_steps          VARCHAR(1500),
-  version                   VARCHAR(200),
-  assigned_to_name          TEXT(30) NOT NULL,
-  priority                  TEXT(10) NOT NULL,
-  FOREIGN KEY (created_by_user_id)     REFERENCES user(id)                 
+  software_version          VARCHAR(200),
+  assigned_to_name          VARCHAR(30) NOT NULL,
+  priority                  VARCHAR(10) NOT NULL,
+  FOREIGN KEY(created_by_user_id)     REFERENCES user_details(id)                 
 );
-
